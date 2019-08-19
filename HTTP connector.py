a530b281e68e57b0a2716df512318ccee0ae8c04
taki138 <tk3225@gmail.com>
2019-08-19 04:34:50 +0300

    2nd commit

--- HTTP connector.py
+++ HTTP connector.py
@@ -1,11 +1,21 @@
 import time
 import requests
 
-def get(url):
+url = 'http://httpbin.org/'
+HTTPMethods = 'get'
+
+
+def get(url, HTTPMethods):
     try:
-        return requests.get(url)
+        print(requests.get(url + HTTPMethods))
+        return requests.get((url + HTTPMethods))
     except Exception:
         # sleep for a bit in case that helps
         time.sleep(1)
         # try again
+        print(requests.get(url))
         return get(url)
+
+
+if __name__ == '__main__':
+    get(url, HTTPMethods)
