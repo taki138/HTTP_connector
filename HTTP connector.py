50d5c2dc2695030bde35e2899041db43cb49d5d2
taki138 <tk3225@gmail.com>
2019-08-19 14:53:32 +0300

    timeout add

--- HTTP connector.py
+++ HTTP connector.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import requests
 from requests.adapters import HTTPAdapter
@@ -5,14 +6,16 @@ from urllib3.util import Retry
 
 # realise Best practice with retries with requests
 url = 'http://httpbin.org/'
-HTTPMethods = 'get'
+HTTPMethods = 'status/500'
 
 
 def requests_retry_session(
-        retries=10,
-        backoff_factor=0.9,
+        retries=6,
+        backoff_factor=0.6,
         status_forcelist=(500, 502, 504),
         session=None,
+        raise_on_status=True
+
 ):
     session = session or requests.Session()
     retry = Retry(
@@ -25,21 +28,26 @@ def requests_retry_session(
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
+    print(retry)
+    print(adapter)
     return session
 
 
 t0 = time.time()
 try:
     response = requests_retry_session().get(
-        url+HTTPMethods,
+        url + HTTPMethods,
+        timeout=5
     )
+
+    print(f'\t Function requests_retry_session: started')
 except Exception as x:
-    print('It failed :(', x.__class__.__name__)
+    print(f'\t Function requests_retry_session error occurred: \n \t {x}')
 else:
-    print('It eventually worked', response.status_code)
+    print('\t Function: requests_retry_session: \n \t response.status_code', response.status_code)
 finally:
     t1 = time.time()
-    print('Took', t1 - t0, 'seconds')
+    print('\t Function requests_retry_session worked time:', t1 - t0, 'seconds')
 
 # if __name__ == '__main__':
 #     # requests_retry_session()
@@ -48,5 +56,3 @@ finally:
 #     s.headers.update({'x-test': 'true'})
 
 # response = requests_retry_session(session=s).get(url + HTTPMethods)
-
-
