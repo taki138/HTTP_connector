45f9a960ac02f72ff0517c66df35765e7332bb7c
taki138 <tk3225@gmail.com>
2019-09-01 04:47:16 +0300

    decorators

--- functions kit.py
+++ functions kit.py
@@ -1,56 +1,37 @@
 import tkinter as tk
 
 
-# def identity(x):
-#     '''Identity function'''
-#     return x
-#
-#
-# f = identity
-# for attribute in (f.__name__,
-#                   f.__doc__,
-#                   f.__module__,
-#                   f.__class__):
-#     print(attribute)
+def identity(x):
+    '''Identity function'''
+    return x
 
-# add_l = lambda x, y: x + y
+
+f = identity
+for attribute in (f.__name__,
+                  f.__doc__,
+                  f.__module__,
+                  f.__class__):
+    print(attribute)
+######### lambda  #########
+add_l = lambda x, y: x + y
 # print(add_l(1, 120))
-#
-#
-# def under_lambda(x, y):
-#     return add_l(x, y)
+
+def under_lambda(x, y):
+    return add_l(x, y)
 # print(under_lambda(8, 3))
 
-class App:
-    """"""
+colors = ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
 
-    def __init__(self, parent):
-        """Конструктор"""
-        frame = tk.Frame(parent)
-        frame.pack()
-
-        btn22 = tk.Button(
-            frame,
-            text="22",
-            command=lambda: self.printNum(22)
-        )
-        btn22.pack(side=tk.LEFT)
-
-        btn44 = tk.Button(
-            frame,
-            text="44",
-            command=lambda: self.printNum(44)
-        )
-        btn44.pack(side=tk.LEFT)
-
-        quitBtn = tk.Button(frame, text="QUIT", fg="red", command=frame.quit)
-        quitBtn.pack(side=tk.LEFT)
-
-    def printNum(self, num):
-        print("Вы нажали кнопку: %s" % num)
+def normalize_case(string):
+    return string.casefold()
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+# normalized_colors = map(normalize_case, colors)
+normalized_colors = map(lambda s: s.casefold(), colors)
+# print(lambda s: s.casefold(), colors)
+
+
+recent_presidents = ['Борис Ельцин', 'Владимир Путин', 'Дмитрий Медведев']
+# print('Последними президентами были %s.' % ', '.join(recent_presidents))
+
+######### decorators #########
