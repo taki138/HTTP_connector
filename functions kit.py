d5f63e27d692125424fdf1fe4924fa5a5cf2ea5f
taki138 <tk3225@gmail.com>
2019-09-01 05:30:44 +0300

    funct_excecution_timer(funct)

--- functions kit.py
+++ functions kit.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from timeit import default_timer as timer
 
 
 def identity(x):
@@ -7,20 +8,26 @@ def identity(x):
 
 
 f = identity
-for attribute in (f.__name__,
-                  f.__doc__,
-                  f.__module__,
-                  f.__class__):
-    print(attribute)
+# for attribute in (f.__name__,
+#                   f.__doc__,
+#                   f.__module__,
+#                   f.__class__):
+#     print(attribute)
+
 ######### lambda  #########
 add_l = lambda x, y: x + y
+
+
 # print(add_l(1, 120))
 
 def under_lambda(x, y):
     return add_l(x, y)
+
+
 # print(under_lambda(8, 3))
 
 colors = ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
+
 
 def normalize_case(string):
     return string.casefold()
@@ -32,6 +39,24 @@ normalized_colors = map(lambda s: s.casefold(), colors)
 
 
 recent_presidents = ['Борис Ельцин', 'Владимир Путин', 'Дмитрий Медведев']
+
+
 # print('Последними президентами были %s.' % ', '.join(recent_presidents))
 
 ######### decorators #########
+def makeitalic(str='Test'):
+    def wrapped():
+        return "<i>" + str + "</i>"
+
+    return wrapped
+
+
+def funct_excecution_timer(funct):
+    start = timer()
+    funct
+    end = timer()
+    print(f'Function {funct.__name__} excecuted time: {end - start}')
+    return funct
+
+
+funct_excecution_timer(makeitalic(str='Test'))
