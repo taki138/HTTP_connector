8d3a7f2e05173ae631ba6750d8f5f616cc337e8c
taki138 <tk3225@gmail.com>
2019-09-03 21:07:40 +0300

    jsonplaceholder

--- JSON.py
+++ JSON.py
@@ -1,14 +1,18 @@
 import json
 
-print(json.dumps({"name": "John", "age": 30}))
-print(json.dumps(["apple", "bananas"]))
-print(json.dumps(("apple", "bananas")))
-print(json.dumps("hello"))
-print(json.dumps(42))
-print(json.dumps(31.76))
-print(json.dumps(True))
-print(json.dumps(False))
-print(json.dumps(None))
+import flask
+import requests
+from flask import jsonify
+
+# print(json.dumps({"name": "John", "age": 30}))
+# print(json.dumps(["apple", "bananas"]))
+# print(json.dumps(("apple", "bananas")))
+# print(json.dumps("hello"))
+# print(json.dumps(42))
+# print(json.dumps(31.76))
+# print(json.dumps(True))
+# print(json.dumps(False))
+# print(json.dumps(None))
 
 
 
@@ -27,3 +31,8 @@ x = {
 }
 
 # print(json.dumps(x, indent=2))
+
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text)
+
+print(todos[:2])
