300881fb83f5ad8c8aa3604461ac19dd8f698eab
taki138 <tk3225@gmail.com>
2019-09-03 19:51:40 +0300

    dic_vlues_to_string UPD

--- sets.py
+++ sets.py
@@ -5,22 +5,27 @@
 my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
 
 for elm in my_dict:
+    pass
     # При таком обходе словаря, перебираются только ключи
     # равносильно for elm in my_dict.keys()
-    print(elm)
+    # print(elm)
 
 for elm in my_dict.values():
+    pass
     # При желании можно пройти только по значениям
-    print(elm)
+    # print(elm)
 
 for key, value in my_dict.items():
+    pass
     # Проход по .items() возвращает кортеж (ключ, значение),
     # который присваивается кортежу переменных key, value
-    print(key, value)
+    # print(key, value)
 
 def dic_vlues_to_string(my_dict: dict) -> str:
-    for key, value in my_dict.items():
-        # Проход по .items() возвращает кортеж (ключ, значение),
-        # который присваивается кортежу переменных key, value
-        print(key, value)
+    tempList = []
+    for elm in my_dict.values():
+        tempList.append(str(elm))
+    strValues=','.join(tempList)
+
+    return strValues
 
